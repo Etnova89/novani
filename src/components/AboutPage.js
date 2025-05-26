af191397c6601aa96df44fe877535cff9c24953a
Eric Terranova <terranova0689@gmail.com>
2025-05-25 20:36:01 -0400

    icon, tab header, and headshot

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -18,7 +18,8 @@ import {
   HealthAndSafety,
 } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
-import { useAppTheme } from '../theme/hooks';
+import { useAppTheme } from "../theme/hooks";
+import HeenaHeadshot from "../assets/images/Heena_headshot_fall2023.jpg";
 
 const AboutPage = () => {
   const { theme, isMobile } = useAppTheme();
@@ -38,23 +39,42 @@ const AboutPage = () => {
           <Grid item xs={12} md={4}>
             <Box
               component="img"
-              src="/images/heena-manglani-terranova.jpg"
+              src={HeenaHeadshot}
               alt="Dr. Heena Manglani-Terranova"
               sx={{
                 width: "100%",
-                borderRadius: "50%",
-                border: `4px solid ${theme.palette.primary.main}`,
+                border: `1px solid ${theme.palette.primary.main}`,
               }}
             />
           </Grid>
           <Grid item xs={12} md={8}>
             <Typography variant="body1" paragraph>
-              Dr. Heena Manglani-Terranova is a Harvard-trained licensed clinical psychologist with a passion for helping patients live a life based on their own personally-chosen values. She believes that life will always be full of stressors that will create distress (i.e., anxiety, depression, worry). That is not the problem. The issue is what we, as humans tend to do when experiencing distress that matters. She uses a behavioral approach to help patients live a life that aligns with what matters most to them. Dr. Manglani-Terranova also specializes in working with South Asian Americans and bicultural couples given her understanding of the unique challenges faced by the South
-              Asian community. Dr. Manglani-Terranova uses a culturally sensitive and curious approach to understand the barriers to living a meaningful life, and provides culturally-attuned care that leverages narratives of strength and resilience in her patients and provides directions forv values-based living.
+              Dr. Heena Manglani-Terranova is a Harvard-trained licensed
+              clinical psychologist with a passion for helping patients live a
+              life based on their own personally-chosen values. She believes
+              that life will always be full of stressors that will create
+              distress (i.e., anxiety, depression, worry). That is not the
+              problem. The issue is what we, as humans tend to do when
+              experiencing distress that matters. She uses a behavioral approach
+              to help patients live a life that aligns with what matters most to
+              them. Dr. Manglani-Terranova also specializes in working with
+              South Asian Americans and bicultural couples given her
+              understanding of the unique challenges faced by the South Asian
+              community. Dr. Manglani-Terranova uses a culturally sensitive and
+              curious approach to understand the barriers to living a meaningful
+              life, and provides culturally-attuned care that leverages
+              narratives of strength and resilience in her patients and provides
+              directions forv values-based living.
             </Typography>
             <Typography variant="body1">
               In addition to her clinical work, Dr. Manglani-Terranova is a
-              research scientist, studying ways to improve health and well-being in South Asian Americans and individuals adjusting to chronic illness (i.e. multiple sclerosis). Her unique focus on relational factors positions her to study individuals within the larger systems in which they function and to leveral a social justice lens to bridge the gap between mental health and chronic disease and holistic care.
+              research scientist, studying ways to improve health and well-being
+              in South Asian Americans and individuals adjusting to chronic
+              illness (i.e. multiple sclerosis). Her unique focus on relational
+              factors positions her to study individuals within the larger
+              systems in which they function and to leveral a social justice
+              lens to bridge the gap between mental health and chronic disease
+              and holistic care.
             </Typography>
           </Grid>
         </Grid>
@@ -132,7 +152,8 @@ const AboutPage = () => {
 
       <Box sx={{ textAlign: "center", mt: 6 }}>
         <Typography variant="h5" gutterBottom>
-          Ready to start your journey with Dr. Manglani-Terranova to uncover your resilience?
+          Ready to start your journey with Dr. Manglani-Terranova to uncover
+          your resilience?
         </Typography>
         <Button
           variant="contained"
