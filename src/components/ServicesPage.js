f46a2dd519fea98cd2bab3454dcde84172a216e2
heena-manglani <heena.r.manglani@gmail.com>
2025-08-26 13:16:22 -0700

    consolidated text for services page

--- src/components/ServicesPage.js
+++ src/components/ServicesPage.js
@@ -10,21 +10,15 @@ const ServicesPage = () => {
       description: (
         <>
           Each person carries a set of{" "}
-          <strong>personal stories about who they are.</strong> Usually these
-          stories are shaped by powerful moments and past experiences. Over
+          <strong>personal stories about who they are. </strong>           
+          Stories are shaped by life experiences. Over
           time, these stories can begin to define what we believe we're capable
-          of, often leading to patterns that feel limiting or stuck. In
-          individual therapy, I work collaboratively with clients to{" "}
-          <strong>identify these recurring “frequent flyer” stories,</strong>{" "}
-          understand how they react to them, and{" "}
-          <strong>
-            develop skills to respond in ways that reflect their values
-          </strong>{" "}
-          and what kind of person they strive to be. Together, we create space
-          for{" "}
-          <strong>
-            greater self-awareness, flexibility, and a more empowered way of
-            living.
+          of, often leading to feeling stuck. In one-on-one sessions, I work collaboratively with patients to{" "}
+          <strong>recognize their life-limiting stories</strong>{" "}
+          and their typical reactions to them.{" "}
+          We then practice skills that enable the individual to respond in ways that reflect their values
+          and the kind of person they want to be. Together, we create space
+          for{" "} <strong> self-awareness, flexibility, and an empowered way of living.
           </strong>
         </>
       ),
@@ -38,22 +32,14 @@ const ServicesPage = () => {
       title: "Couple Therapy",
       description: (
         <>
-          Every couple has a familiar “dance” during conflict—often a cycle
-          where one partner criticizes, the other defends or counter-attacks,
-          and eventually someone withdraws or shuts down. In couple therapy, one
-          of <strong>the first steps is recognizing this dance,</strong>{" "}
-          learning to pause, and listening for the music underneath—the
-          emotions, needs, and longings that drive it. Together, we work to{" "}
-          <strong>co-create new patterns of interaction</strong> that help you
-          move more intentionally and in sync with one another. These new
-          patterns are particularly important as couples go through life
-          transitions--making a long-term commitment to each other, parenthood,
-          midlife demands of parenting and caregiving, becoming grandparents,
-          retirement, and aging. Sessions focus on deepening understanding,
-          improving communication, and strengthening both partners’ ability to
-          navigate conflict together. At the core of the work is a commitment to
-          building a partnership rooted in{" "}
-          <strong>gratitude, compassion, and connection.</strong>
+          Every couple has a familiar “dance” during conflict. Often this is a cycle wherein one one partner
+          criticizes, the other defends, and eventually one or both withdraw. In couple therapy, one of <strong>the first steps is recognizing this dance,</strong>{" "}
+          learning to pause, and listening for the
+          emotions and needs that drive the conflict. We work to{" "}
+          <strong>co-create new patterns of interaction</strong> that address both partners' needs. These new
+          patterns are particularly important during life
+          transitions--marriage, parenthood, caregiving, retirement, illness, and aging. Not only do we focus on healthy conflict resolution and adapting to life changes, we also harness evidence-based methods to strengthen a culture of fondness and admiration. The overall goal is to help re-establish a partnership of{" "}
+          <strong> connection, compassion, and gratitude.</strong>
         </>
       ),
       details: [
@@ -61,20 +47,19 @@ const ServicesPage = () => {
         "Gottman Method",
         "Narrative Therapy",
       ],
+      image: "Heena_Chair.jpg",
     },
     {
       title: "Family Therapy",
       description: (
         <>
           <strong>
-            Family conflict is rarely just about individual disagreements
-          </strong>
-          —it is rooted in the broader systems and intergenerational patterns
-          that shape each family member's experience. Families are dynamic and
+            Family conflict is rarely just about individual disagreements.
+          </strong> Families are dynamic and
           ever-evolving, much like the seasons. Family tension often arises
           during natural transitions—such as adolescents becoming adults, adult
           children stepping into parenting roles, or major life changes like
-          chronic illness or loss affecting the family system. These periods
+          chronic illness or loss. These periods
           call for shifts in roles and expectations, which can sometimes be met
           with resistance or difficulty. When families struggle to adapt, one or
           more members may feel unseen, misunderstood, or even oppressed. In
@@ -83,13 +68,10 @@ const ServicesPage = () => {
           and fostering more effective and compassionate ways of relating.
           Together, we explore how shifts in one part of the system can create
           ripple effects throughout the family. This work is about more than
-          resolving conflict, it's about{" "}
+          resolving conflict, it's about
           <strong>
-            helping each member feel more connected, understood, and empowered.
+            helping each member feel more seen, connected, and empowered.
           </strong>{" "}
-          My approach supports the well-being of both individuals and the family
-          unit as a whole, while acknowledging the larger social, cultural, and
-          relational systems in which they live.
         </>
       ),
       details: [
@@ -104,35 +86,28 @@ const ServicesPage = () => {
         <>
           <strong>Coaching:</strong> I offer coaching services for individuals
           seeking personal or professional growth without a mental health
-          diagnosis. Coaching focuses on building your vision, confidence, and
-          actionable strategies to help you achieve your goals—whether that’s{" "}
+          diagnosis. Coaching focuses on building the client's vision, confidence, and
+          actionable strategies to help achieve their goals. This can range from{" "}
           <strong>
             improving communication, navigating life transitions, or enhancing
-            leadership skills.
-          </strong>{" "}
-          Unlike therapy, coaching is future-oriented and goal-driven, helping
-          you move from where you are to where you want to be.{" "}
-          <strong>My approach is culturally sensitive and inclusive,</strong>{" "}
+            leadership skills.</strong>{" "} My approach is culturally sensitive and inclusive,
           recognizing how factors such as culture, race, gender, family
-          dynamics, and life experiences shape your goals and challenges.
-          Sessions are collaborative, strengths-based, and designed to empower
-          you while respecting the diverse contexts that make you who you are.{" "}
+          dynamics, and life experiences shape goals and challenges.
+          Sessions are <strong>{" "} collaborative, strengths-based, and designed to empower
+          the client </strong>{" "} while respecting the diverse contexts that make them who they are.
           <br />
           <br />
-          <strong>Consulting:</strong> I also offer consulting services for
+          <strong>Consulting:</strong> I offer consulting services for
           organizations and teams seeking to{" "}
           <strong>
-            strengthen well-being, communication, and performance in their work
-            environments.
-          </strong>{" "}
-          I also offer speaking engagements and interactive workshops on topics
-          such as stress management, health & wellness, and team collaboration.
+            strengthen well-being, communication, and performance
+            </strong>{" "} in their work environments. Through speaking engagements and interactive workshops, I provide education and skill-building on topics such as stress management, health & wellness, and team collaboration.
           Each session blends{" "}
-          <strong>science-backed strategies with practical tools</strong>
+          <strong>science-backed strategies with practical tools </strong>
           that participants can immediately apply in their personal and
           professional lives. Each engagement is{" "}
-          <strong>tailored to your organization’s goals,</strong> whether you’re
-          looking for a keynote presentation, a leadership training, or an
+          <strong>tailored to the organization’s goals,</strong> whether it's for a
+          keynote presentation, leadership training, or an
           immersive workshop experience
         </>
       ),
@@ -185,25 +160,16 @@ const ServicesPage = () => {
               </Typography>
               <Box sx={{ mt: 2, mb: 3 }}>
                 <Typography variant="h6" gutterBottom>
-                  Modalities, Approaches, & Topics:
+                  Approaches & Topics:
                 </Typography>
                 <ul>
                   {service.details.map((detail, idx) => (
                     <li key={idx}>
-                      <Typography variant="body2">{detail}</Typography>
+                      <Typography variant="body1">{detail}</Typography>
                     </li>
                   ))}
                 </ul>
               </Box>
-              <Button
-                variant="outlined"
-                color="secondary"
-                endIcon={<ArrowForward />}
-                sx={{ mt: "auto", alignSelf: "flex-start" }}
-                onClick={handleConsultationClick}
-              >
-                Learn More
-              </Button>
             </Paper>
           </Grid>
         ))}
